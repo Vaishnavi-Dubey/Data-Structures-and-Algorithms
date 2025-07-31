ce3e42015a07805372358c97790f5273b9ef3e67
Vaishnavi Dubey <vaishnavidubey1290@gmail.com>
2025-07-31 23:53:52 +0530

    DataTypes.cpp

---
+++ CppBasics/DataTypes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//int
+	int x = 10;
+	// long
+	long x = 15
+  cin >> x;
+
+	long long x = 150000000;
+
+	// float, double
+	float x = 5.6;
+
+	return 0;
+}
